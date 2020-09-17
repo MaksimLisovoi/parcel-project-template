db805b1c8b22628e3fdd95b7f1a4d0daf116dae6
Katerina Kucheriavenko <ekucheryavenko0925@gmail.com>
2020-09-18 01:39:00 +0300

    Добавляет скрипты и позиционирует стрелки

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $('.slick-slider').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+    pauseOnHover: true,
+  });
+});
