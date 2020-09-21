00ea30d00cd22e8aea46aaf6a505a81db7884261
Maksim <lisovoi123@gmail.com>
2020-09-21 15:12:17 +0300

    Меняет скрипт для меню

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,25 @@
 (() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const refs = {
+    openModalBtn: document.querySelector('[data-menu-open]'),
+    closeModalBtn: document.querySelector('[data-menu-close]'),
+    modal: document.querySelector('[data-menu]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('is-open');
+  }
+})();
+
+(() => {
+  const menuNavLink = document.querySelector('[data-menu-link]');
+  const menuBtnRef = document.querySelector('[data-menu-close]');
   const mobileMenuRef = document.querySelector('[data-menu]');
-
-  menuBtnRef.addEventListener('click', () => {
-    const expanded =
-      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-    menuBtnRef.classList.toggle('is-open');
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
-
+  menuNavLink.addEventListener('click', () => {
     mobileMenuRef.classList.toggle('is-open');
+    menuBtnRef.classList.toggle('is-open');
   });
 })();
